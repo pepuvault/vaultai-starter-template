b95d1854c53305c61371ddb74c170fd6f32592f8
pepuvault <pepuvault@gmail.com>
2025-07-21 16:18:00 +0200

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>🎉 VaultAI ist live!</h1>
-      <p>Diese App wurde erfolgreich auf Vercel deployed.</p>
-    </main>
-  );
-}
